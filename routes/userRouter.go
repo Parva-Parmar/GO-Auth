51484c74453b967a6a56683d3d9e9f6c73dc4e75
Parva Parmar <parva004cp@gmail.com>
2024-06-21 20:23:55 +0530

    update databaseconnection.go (deprecated)

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	controller "github.com/Parva-Parmar/GO-Auth/controlllers"
+	"github.com/Parva-Parmar/GO-Auth/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incommingRoutes *gin.Engine){
+	incommingRoutes.Use(middleware.Authenticate())
+	incommingRoutes.GET("/users",controller.GetUsers())
+	incommingRoutes.GET("/users/:user_id",controller.GetUsers())
+}

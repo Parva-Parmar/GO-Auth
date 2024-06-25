5f3627587f36bd3a3a0d1fb4aaaabc994f9a7468
Parva Parmar <parva004cp@gmail.com>
2024-06-25 19:05:23 +0530

    Update authHelper.go MatchUserTypetoUid function

---
+++ helpers/authHelper.go
@@ -0,0 +1,23 @@
+package helper
+
+import(
+	"errors"
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context,role string)(err error){
+	
+}
+
+func MatchUserTypetoUid(c *gin.Context,userId string) (err error){
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorizedd to access this resource")
+		return err
+	}
+	err = CheckUserType(c,userType)
+	return err
+}

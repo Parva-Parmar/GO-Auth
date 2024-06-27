d3fbd8cc12ebd92e089c2ae22303424e06863e91
Parva Parmar <parva004cp@gmail.com>
2024-06-27 18:19:06 +0530

    Update Controlller.go Signup function

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -14,6 +14,7 @@ import (
 	"github.com/Parva-Parmar/GO-Auth/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -25,7 +26,41 @@ func HashPassword()
 
 func VerifyPassord()
 
-func Signup()
+func Signup() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+		var user models.User 
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest,gin.H{"error":validationErr.Error()})
+			return
+		}
+
+		count,err := UserCollection.CountDocuments(ctx,bson.M{"email":user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured while checking for email"})
+		}
+
+		count,err = UserCollection.CountDocuments(ctx,bson.M{"phone":user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured  while checking for the Phone"})
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"this email of phone number already exists"})
+		}
+	}
+}
 
 func Login()
 

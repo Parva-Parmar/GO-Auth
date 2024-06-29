17046b4d3223555b267abafd47f10d8da53d3893
Parva Parmar <parva004cp@gmail.com>
2024-06-29 19:46:06 +0530

    updated Login and verifypassword functions

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -17,6 +17,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/check.v1"
 )
 
 var UserCollection *mongo.Collection = database.OpenCollection(database.Client,"user")
@@ -24,7 +25,15 @@ var validate = validator.New()
 
 func HashPassword()
 
-func VerifyPassord()
+func VerifyPassord(userPassword string,providedPassword string)(bool,string){
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword),[]byte(userPassword))
+	check := true
+	msg := ""
+	if err != nil {
+		msg = fmt.Sprintf("email or password is incorrect")
+		check = false
+	}
+}
 
 func Signup() gin.HandlerFunc{
 	return func(c *gin.Context) {
@@ -49,7 +58,7 @@ func Signup() gin.HandlerFunc{
 			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured while checking for email"})
 		}
 
-		count,err := UserCollection.CountDocuments(ctx,bson.M{"phone":user.Phone})
+		count,err = UserCollection.CountDocuments(ctx,bson.M{"phone":user.Phone})
 		defer cancel()
 		if err != nil {
 			log.Panic(err)
@@ -79,7 +88,28 @@ func Signup() gin.HandlerFunc{
 	}
 }
 
-func Login()
+func Login() gin.HandlerFunc{
+	return func(c *gin.Context){
+		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+			return
+		}
+
+		err := UserCollection.FindOne(ctx,bson.M{"email":user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"email of password is incorrect"})
+			return
+		}
+
+		passwordIsValid,msg := VerifyPassord(*user.Password,*foundUser.Password)
+		defer cancel()
+	}
+}
 
 func GetUsers()
 
